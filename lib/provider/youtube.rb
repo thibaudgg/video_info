4c3e6f75a2a4c08bc2890c5818616e342762269a
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-04-17 13:42:45 +0200

    Definitely fixed YouTube thumbnails sizes

--- lib/provider/youtube.rb
+++ lib/provider/youtube.rb
@@ -6,14 +6,14 @@ class Youtube
                 :duration, :date, :width, :height,
                 :thumbnail_small, :thumbnail_large,
                 :view_count
-  
+
   def initialize(url)
     @video_id = url.gsub(/.*v=([^&]+).*$/i, '\1')
     get_info unless @video_id == url
   end
-  
+
 private
-  
+
   def get_info
     doc = Hpricot(open("http://gdata.youtube.com/feeds/api/videos/#{@video_id}"))
     @provider         = "YouTube"
@@ -23,15 +23,15 @@ private
     @keywords         = doc.search("media:keywords").inner_text
     @duration         = doc.search("yt:duration").first[:seconds].to_i
     @date             = Time.parse(doc.search("published").inner_text, Time.now.utc)
-    @thumbnail_small  = doc.search("media:thumbnail").last[:url]
-    @thumbnail_large  = doc.search("media:thumbnail").first[:url]
+    @thumbnail_small  = doc.search("media:thumbnail").min { |a,b| a[:height].to_i * a[:width].to_i <=> b[:height].to_i * b[:width].to_i }[:url]
+    @thumbnail_large  = doc.search("media:thumbnail").max { |a,b| a[:height].to_i * a[:width].to_i <=> b[:height].to_i * b[:width].to_i }[:url]
     # when your video still has no view, yt:statistics is not returned by Youtube
     # see: https://github.com/thibaudgg/video_info/issues#issue/2
     if doc.search("yt:statistics").first
       @view_count     = doc.search("yt:statistics").first[:viewcount].to_i
     else
-      @view_count     = 0 
+      @view_count     = 0
     end
   end
-  
+
 end
--- spec/video_info_spec.rb
+++ spec/video_info_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe "VideoInfo" do
-  
+
   context "from Youtube" do
     describe "Video mZqGqE0D0n4" do
       use_vcr_cassette "youtube/mZqGqE0D0n4"
       subject { VideoInfo.new('http://www.youtube.com/watch?v=mZqGqE0D0n4') }
-    
+
       its(:provider)         { should == 'YouTube' }
       its(:video_id)         { should == 'mZqGqE0D0n4' }
       its(:url)              { should == 'http://www.youtube.com/watch?v=mZqGqE0D0n4' }
@@ -17,34 +17,34 @@ describe "VideoInfo" do
       its(:width)            { should be_nil }
       its(:height)           { should be_nil }
       its(:date)             { should == Time.parse('Sat Apr 12 22:25:35 UTC 2008', Time.now.utc) }
-      its(:thumbnail_small)  { should == 'http://i.ytimg.com/vi/mZqGqE0D0n4/2.jpg' }
+      its(:thumbnail_small)  { should == 'http://i.ytimg.com/vi/mZqGqE0D0n4/1.jpg' }
       its(:thumbnail_large)  { should == 'http://i.ytimg.com/vi/mZqGqE0D0n4/0.jpg' }
       its(:view_count)       { should be > 4000 }
       it { should be_valid }
     end
-    
+
     describe "Video JM9NgvjjVng" do
       use_vcr_cassette "youtube/JM9NgvjjVng"
       subject { VideoInfo.new('http://www.youtube.com/watch?v=JM9NgvjjVng') }
-    
+
       its(:provider)         { should == 'YouTube' }
       its(:duration)         { should == 217 }
       it { should be_valid }
     end
-    
+
     describe "Video oQ49W_xKzKA" do
       use_vcr_cassette "youtube/oQ49W_xKzKA"
       subject { VideoInfo.new('http://www.youtube.com/watch?v=oQ49W_xKzKA') }
-    
-      its(:view_count) { should == 0 }
+
+      its(:view_count) { should == 2 }
     end
   end
-  
+
   context "from Vimeo" do
     describe "Video 898029" do
       use_vcr_cassette "vimeo/898029"
       subject { VideoInfo.new('http://www.vimeo.com/898029') }
-    
+
       its(:provider)         { should == 'Vimeo' }
       its(:video_id)         { should == '898029' }
       its(:url)              { should == 'http://vimeo.com/898029' }
@@ -61,28 +61,28 @@ describe "VideoInfo" do
       it { should be_valid }
     end
   end
-  
-  context "from" do  
+
+  context "from" do
     describe "misstaped url" do
       subject { VideoInfo.new('http://www.vimo.com/1') }
-      
+
       it { should_not be_valid }
     end
     describe "bad url" do
       subject { VideoInfo.new('http://www.yasda.com/asdasd') }
-      
+
       it { should_not be_valid }
     end
     describe "blank url" do
       subject { VideoInfo.new('') }
-      
+
       it { should_not be_valid }
     end
     describe "nil url" do
       subject { VideoInfo.new(nil) }
-      
+
       it { should_not be_valid }
     end
   end
-  
+
 end
