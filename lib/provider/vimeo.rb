e4e57f9cb256b7eadf3015b9f8ae7090b1514c8f
Simon Ernst <se@netmute.org>
2012-05-30 15:00:27 +0200

    Naive error handling.

--- lib/provider/vimeo.rb
+++ lib/provider/vimeo.rb
@@ -8,27 +8,31 @@ class Vimeo
   def initialize(url, options = {})
     @openURI_options = options
     @video_id = url.gsub(/.*\.com\/(?:groups\/[^\/]+\/videos\/)?([0-9]+).*$/i, '\1')
-    get_info unless @video_id == url
+    get_info unless @video_id == url || @video_id.nil? || @video_id.empty?
   end
 
 private
 
   def get_info
-    doc = Hpricot(open("http://vimeo.com/api/v2/video/#{@video_id}.xml", @openURI_options))
-    @provider         = "Vimeo"
-    @url              = doc.search("url").inner_text
-    @embed_url        = "http://player.vimeo.com/video/#{@video_id}"
-    @embed_code       = "<iframe src=\"#{@embed_url}?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0\" frameborder=\"0\"></iframe>"
-    @title            = doc.search("title").inner_text
-    @description      = doc.search("description").inner_text
-    @keywords         = doc.search("tags").inner_text
-    @duration         = doc.search("duration").inner_text.to_i # seconds
-    @width            = doc.search("width").inner_text.to_i
-    @height           = doc.search("height").inner_text.to_i
-    @date             = Time.parse(doc.search("upload_date").inner_text, Time.now.utc).utc
-    @thumbnail_small  = doc.search("thumbnail_small").inner_text
-    @thumbnail_large  = doc.search("thumbnail_large").inner_text
-    @view_count       = doc.search("stats_number_of_plays").inner_text.to_i
+    begin
+      doc = Hpricot(open("http://vimeo.com/api/v2/video/#{@video_id}.xml", @openURI_options))
+      @provider         = "Vimeo"
+      @url              = doc.search("url").inner_text
+      @embed_url        = "http://player.vimeo.com/video/#{@video_id}"
+      @embed_code       = "<iframe src=\"#{@embed_url}?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=0\" frameborder=\"0\"></iframe>"
+      @title            = doc.search("title").inner_text
+      @description      = doc.search("description").inner_text
+      @keywords         = doc.search("tags").inner_text
+      @duration         = doc.search("duration").inner_text.to_i # seconds
+      @width            = doc.search("width").inner_text.to_i
+      @height           = doc.search("height").inner_text.to_i
+      @date             = Time.parse(doc.search("upload_date").inner_text, Time.now.utc).utc
+      @thumbnail_small  = doc.search("thumbnail_small").inner_text
+      @thumbnail_large  = doc.search("thumbnail_large").inner_text
+      @view_count       = doc.search("stats_number_of_plays").inner_text.to_i
+    rescue
+      nil
+    end
   end
 
 end
--- lib/provider/youtube.rb
+++ lib/provider/youtube.rb
@@ -8,7 +8,7 @@ class Youtube
   def initialize(url, options = {})
     @openURI_options = options
     video_id_for(url)
-    get_info unless @video_id == url
+    get_info unless @video_id == url || @video_id.nil? || @video_id.empty?
   end
 
   def regex
@@ -24,24 +24,28 @@ class Youtube
 private
 
   def get_info
-    doc = Hpricot(open("http://gdata.youtube.com/feeds/api/videos/#{@video_id}", @openURI_options))
-    @provider         = "YouTube"
-    @url              = "http://www.youtube.com/watch?v=#{@video_id}"
-    @embed_url        = "http://www.youtube.com/embed/#{@video_id}"
-    @embed_code       = "<iframe src=\"#{@embed_url}\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe>"
-    @title            = doc.search("media:title").inner_text
-    @description      = doc.search("media:description").inner_text
-    @keywords         = doc.search("media:keywords").inner_text
-    @duration         = doc.search("yt:duration").first[:seconds].to_i
-    @date             = Time.parse(doc.search("published").inner_text, Time.now.utc)
-    @thumbnail_small  = doc.search("media:thumbnail").min { |a,b| a[:height].to_i * a[:width].to_i <=> b[:height].to_i * b[:width].to_i }[:url]
-    @thumbnail_large  = doc.search("media:thumbnail").max { |a,b| a[:height].to_i * a[:width].to_i <=> b[:height].to_i * b[:width].to_i }[:url]
-    # when your video still has no view, yt:statistics is not returned by Youtube
-    # see: https://github.com/thibaudgg/video_info/issues#issue/2
-    if doc.search("yt:statistics").first
-      @view_count     = doc.search("yt:statistics").first[:viewcount].to_i
-    else
-      @view_count     = 0
+    begin
+      doc = Hpricot(open("http://gdata.youtube.com/feeds/api/videos/#{@video_id}", @openURI_options))
+      @provider         = "YouTube"
+      @url              = "http://www.youtube.com/watch?v=#{@video_id}"
+      @embed_url        = "http://www.youtube.com/embed/#{@video_id}"
+      @embed_code       = "<iframe src=\"#{@embed_url}\" frameborder=\"0\" allowfullscreen=\"allowfullscreen\"></iframe>"
+      @title            = doc.search("media:title").inner_text
+      @description      = doc.search("media:description").inner_text
+      @keywords         = doc.search("media:keywords").inner_text
+      @duration         = doc.search("yt:duration").first[:seconds].to_i
+      @date             = Time.parse(doc.search("published").inner_text, Time.now.utc)
+      @thumbnail_small  = doc.search("media:thumbnail").min { |a,b| a[:height].to_i * a[:width].to_i <=> b[:height].to_i * b[:width].to_i }[:url]
+      @thumbnail_large  = doc.search("media:thumbnail").max { |a,b| a[:height].to_i * a[:width].to_i <=> b[:height].to_i * b[:width].to_i }[:url]
+      # when your video still has no view, yt:statistics is not returned by Youtube
+      # see: https://github.com/thibaudgg/video_info/issues#issue/2
+      if doc.search("yt:statistics").first
+        @view_count     = doc.search("yt:statistics").first[:viewcount].to_i
+      else
+        @view_count     = 0
+      end
+    rescue
+      nil
     end
   end
 
