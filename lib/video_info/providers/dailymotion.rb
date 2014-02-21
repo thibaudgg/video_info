06dafae5b4563ac9dde0909e34327f103b001714
Vincent Pochet <vincent.pochet@gmail.com>
2014-02-21 16:43:07 +0100

    Add dailymotion provider

---
+++ lib/video_info/providers/dailymotion.rb
@@ -0,0 +1,85 @@
+class VideoInfo
+  module Providers
+    class Dailymotion < Provider
+
+      def self.usable?(url)
+        url =~ /(dai(?:\.ly|lymotion\.com))/
+      end
+
+      def provider
+        'Dailymotion'
+      end
+
+      %w[title description duration].each do |method|
+        define_method(method) { data[method] }
+      end
+
+      %w[width height].each do |method|
+        define_method(method) { nil }
+      end
+
+      def keywords
+        data['tags']
+      end
+
+      def embed_url
+        "www.dailymotion.com/embed/video/#{video_id}"
+      end
+
+      def date
+        Time.at(data['created_time']).utc
+      end
+
+      def thumbnail_small
+        data['thumbnail_60_url']
+      end
+
+      def thumbnail_medium
+        data['thumbnail_240_url']
+      end
+
+      def thumbnail_large
+        data['thumbnail_720_url']
+      end
+
+      def view_count
+        data['views_total'].to_i
+      end
+
+      private
+
+      def _response_code
+        response = nil
+        Net::HTTP.start(_api_base, 443, use_ssl: true) {|http|
+          response = http.get(_api_path)
+        }
+        response.code
+      end
+
+      def _url_regex
+        /dai(?:\.ly|lymotion\.com\/(?:embed\/)?video)\/([a-zA-Z0-9]*)/
+      end
+
+      def _api_base
+        "api.dailymotion.com"
+      end
+
+      def _api_path
+        "/video/#{video_id}"
+      end
+
+      def _api_url
+        "https://#{_api_base}#{_api_path}?fields=id,title,description,duration,created_time,url,views_total,tags,thumbnail_url,thumbnail_720_url,thumbnail_240_url,thumbnail_60_url"
+      end
+
+      def _default_iframe_attributes
+        {}
+      end
+
+      def _default_url_attributes
+        { autoplay: 0 }
+      end
+
+    end
+  end
+end
---
+++ spec/lib/video_info/providers/dailymotion_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+describe VideoInfo::Providers::Dailymotion do
+
+  describe ".usable?" do
+    subject { VideoInfo::Providers::Dailymotion.usable?(url) }
+
+    context "with dailymotion url" do
+      let(:url) { 'http://www.dailymotion.com/video/x7lni3' }
+      it { should be_true }
+    end
+
+    context "with dai.ly url" do
+      let(:url) { 'http://dai.ly/video/x7lni3' }
+      it { should be_true }
+    end
+
+    context "with other url" do
+      let(:url) { 'http://www.youtube.com/x7lni3' }
+      it { should be_false }
+    end
+  end
+
+  describe "#available?" do
+    context "with valid video", :vcr do
+      subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
+      its(:available?)       { should be_true }
+    end
+
+    context "with 'This video does not exist or has been deleted.", :vcr do
+      subject { VideoInfo.new('http://www.dailymotion.com/video/inValIdiD') }
+      its(:available?)       { should be_false }
+    end
+  end
+
+  context "with video x7lni3", :vcr do
+    subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
+
+    its(:provider)         { should eq 'Dailymotion' }
+    its(:video_id)         { should eq 'x7lni3' }
+    its(:url)              { should eq 'http://www.dailymotion.com/video/x7lni3' }
+    its(:embed_url)        { should eq 'www.dailymotion.com/embed/video/x7lni3' }
+    its(:embed_code)       { should eq '<iframe src="http://www.dailymotion.com/embed/video/x7lni3?autoplay=0" frameborder="0"></iframe>' }
+    its(:title)            { should eq 'Mario Kart (Rémi Gaillard)' }
+    its(:description)      { should eq 'Super Rémi Kart est un jeu vidéo de course développé et édité par N\'Importe Quoi TV. Sa principale originalité vient de sa réalité...<br />L\'aventure continue sur www.nimportequi.com' }
+    its(:keywords)         { should eq ["rémi","remi","gaillard","imposteur","imposture","mario","bros","kart","nintendo","jeux"] }
+    its(:duration)         { should eq 136}
+    its(:width)            { should be_nil }
+    its(:height)           { should be_nil }
+    its(:date)             { should eq Time.parse('2008-12-03 16:29:31 UTC', Time.now.utc) }
+    its(:thumbnail_small)  { should eq 'http://s2.dmcdn.net/BgWxI/x60-kbf.jpg' }
+    its(:thumbnail_medium) { should eq 'http://s2.dmcdn.net/BgWxI/x240-b83.jpg' }
+    its(:thumbnail_large)  { should eq 'http://s2.dmcdn.net/BgWxI/x720-YcV.jpg' }
+    its(:view_count)       { should be > 10000000 }
+  end
+
+  context "with video x7lni3 and url_attributes", :vcr do
+    subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
+
+    it { expect(subject.embed_code(url_attributes: { autoplay: 1 })).to match(/autoplay=1/) }
+  end
+
+  context "with video x7lni3 and iframe_attributes", :vcr do
+    subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
+
+    it { expect(subject.embed_code(iframe_attributes: { width: 800, height: 600 })).to match(/width="800"/) }
+    it { expect(subject.embed_code(iframe_attributes: { width: 800, height: 600 })).to match(/height="600"/) }
+  end
+
+  context "with video x7lni3 in text", :vcr do
+    subject { VideoInfo.new('<a href="http://www.dailymotion.com/video/x7lni3">http://www.dailymotion.com/video/x7lni3</a>') }
+
+    its(:provider) { should eq 'Dailymotion' }
+    its(:video_id) { should eq 'x7lni3' }
+  end
+
+end
