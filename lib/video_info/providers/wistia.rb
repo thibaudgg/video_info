01c1dedaf9d2fa2b1c556120f3595c7c160659af
fuzzyjulz <julz.west@gmail.com>
2014-12-09 17:29:40 +1100

    Adding in Wistia support and tests

---
+++ lib/video_info/providers/wistia.rb
@@ -0,0 +1,65 @@
+class VideoInfo
+  module Providers
+    class Wistia < Provider
+      
+      def self.usable?(url)
+        url =~ /(.*)(wistia.com|wistia.net|wi.st)/
+      end
+
+      def provider
+        'Wistia'
+      end
+
+      %w[title duration width height].each do |method|
+        define_method(method) { data[method] }
+      end
+
+      %w[description keywords view_count date].each do |method|
+        define_method(method) { nil }
+      end
+
+      def embed_url
+        "//fast.wistia.net/embed/iframe/#{video_id}"
+      end
+
+      def thumbnail_small
+        data['thumbnail_url']
+      end
+
+      def thumbnail_medium
+        data['thumbnail_url']
+      end
+
+      def thumbnail_large
+        data['thumbnail_url']
+      end
+
+      private
+
+      def _url_regex
+        /(?:.*)(?:wistia.com|wi.st|wistia.net)\/(?:embed\/)?(?:medias\/)?(?:iframe\/)?(.+)/
+      end
+
+      def _api_base
+        "fast.wistia.com"
+      end
+
+      def _api_path
+        "/oembed?url=http%3A%2F%2Fhome.wistia.com%2Fmedias%2F#{video_id}"
+      end
+
+      def _api_url
+        "http://#{_api_base}#{_api_path}"
+      end
+
+      def _default_iframe_attributes
+        {}
+      end
+
+      def _default_url_attributes
+        {}
+      end
+
+    end
+  end
+end
