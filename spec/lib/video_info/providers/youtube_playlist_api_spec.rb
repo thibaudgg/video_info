c876c0f980b0235482c2533b58b10f6e3af5c0a1
Vincent Heuken <me@vincentheuken.com>
2015-05-23 08:44:45 -0700

    separated YoutubePlaylistAPI spec from YoutubePlaylist spec

--- spec/lib/video_info/providers/youtube_playlist_api_spec.rb
+++ spec/lib/video_info/providers/youtube_playlist_api_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe VideoInfo::Providers::YoutubePlaylist do
+  before(:all) do
+    VideoInfo.provider_api_keys = { youtube: 'AIzaSyA6PYwSr1EnLFUFy1cZDk3Ifb0rxeJaeZ0' }
+  end
+
   describe ".usable?" do
     subject { VideoInfo::Providers::YoutubePlaylist.usable?(url) }
 
