d5fb1f976bb2ec33401f3ef42418103ca792a86f
Vincent Heuken <me@vincentheuken.com>
2015-05-29 15:40:02 -0700

    implemented description in YoutubePlaylistScraper

--- lib/video_info/providers/youtubeplaylist_scraper.rb
+++ lib/video_info/providers/youtubeplaylist_scraper.rb
@@ -1,7 +1,23 @@
+require 'nokogiri'
+require 'open-uri'
+require 'open_uri_redirections'
+
 class VideoInfo
   module Providers
     module YoutubePlaylistScraper
+      def description
+	data.css('meta')[1].values[1]
+      end
 
+      private
+
+      def _set_data_from_api(api_url = _api_url)
+	Nokogiri::HTML(open(api_url, :allow_redirections => :safe))
+      end
+
+      def _api_url
+	@url
+      end
     end
   end
 end
