933a2fe79de93e1a9c6fcead3a9aeba02831531c
Vincent Heuken <me@vincentheuken.com>
2015-06-01 01:28:31 -0700

    somehow i missed more tabs......

--- lib/video_info/providers/youtubeplaylist_scraper.rb
+++ lib/video_info/providers/youtubeplaylist_scraper.rb
@@ -6,23 +6,23 @@ class VideoInfo
   module Providers
     module YoutubePlaylistScraper
       def description
-	meta_nodes = data.css('meta')
+        meta_nodes = data.css('meta')
 
-	description_node = meta_nodes.detect do 
-	  |m| m.attr('name').value == 'description'
-	end
+        description_node = meta_nodes.detect do 
+          |m| m.attr('name').value == 'description'
+        end
 
-	description_node.attr('content').value
+        description_node.attr('content').value
       end
       
       def title
-	meta_nodes = data.css('meta')
-	
-	title_node = meta_nodes.detect do
-	  |m| m.attr('name').value == 'title'
-	end
-	
-	title_node.attr('content').value
+        meta_nodes = data.css('meta')
+  
+        title_node = meta_nodes.detect do
+          |m| m.attr('name').value == 'title'
+        end
+  
+        title_node.attr('content').value
       end
       
       def videos
@@ -30,29 +30,29 @@ class VideoInfo
       end
       
       def thumbnail_small
-	thumbnail_medium.sub('mqdefault.jpg', 'default.jpg')
+        thumbnail_medium.sub('mqdefault.jpg', 'default.jpg')
       end
 
       def thumbnail_medium
-	'https:' + data.css('div.pl-header-thumb img').attr('src')[0].value
+        'https:' + data.css('div.pl-header-thumb img').attr('src')[0].value
       end
       
       def thumbnail_large
-	thumbnail_medium.sub('mqdefault.jpg', 'hqdefault.jpg')
+        thumbnail_medium.sub('mqdefault.jpg', 'hqdefault.jpg')
       end
 
       private
       
       def available?
-	!data.css('div#page').attr('class')[0].value.include?('oops-content')
+        !data.css('div#page').attr('class')[0].value.include?('oops-content')
       end
      
       def _set_data_from_api(api_url = _api_url)
-	Oga.parse_html(open(api_url, :allow_redirections => :safe))
+        Oga.parse_html(open(api_url, :allow_redirections => :safe))
       end
       
       def _api_url
-	@url
+        @url
       end
     end
   end
