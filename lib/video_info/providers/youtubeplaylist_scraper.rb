69b16f0bdb62a880c7f8c7d5d43dfe1e78bb332d
Vincent Heuken <me@vincentheuken.com>
2015-06-01 01:31:16 -0700

    fixed trailing whitespace...

--- lib/video_info/providers/youtubeplaylist_scraper.rb
+++ lib/video_info/providers/youtubeplaylist_scraper.rb
@@ -8,27 +8,27 @@ class VideoInfo
       def description
         meta_nodes = data.css('meta')
 
-        description_node = meta_nodes.detect do 
+        description_node = meta_nodes.detect do
           |m| m.attr('name').value == 'description'
         end
 
         description_node.attr('content').value
       end
-      
+
       def title
         meta_nodes = data.css('meta')
-  
+
         title_node = meta_nodes.detect do
           |m| m.attr('name').value == 'title'
         end
-  
+
         title_node.attr('content').value
       end
-      
+
       def videos
         raise(NotImplementedError, 'To access videos, you must provide an API key to VideoInfo.provider_api_keys')
       end
-      
+
       def thumbnail_small
         thumbnail_medium.sub('mqdefault.jpg', 'default.jpg')
       end
@@ -36,21 +36,21 @@ class VideoInfo
       def thumbnail_medium
         'https:' + data.css('div.pl-header-thumb img').attr('src')[0].value
       end
-      
+
       def thumbnail_large
         thumbnail_medium.sub('mqdefault.jpg', 'hqdefault.jpg')
       end
 
       private
-      
+
       def available?
         !data.css('div#page').attr('class')[0].value.include?('oops-content')
       end
-     
+
       def _set_data_from_api(api_url = _api_url)
         Oga.parse_html(open(api_url, :allow_redirections => :safe))
       end
-      
+
       def _api_url
         @url
       end
