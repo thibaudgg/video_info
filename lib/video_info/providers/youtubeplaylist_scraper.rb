c6eea45dee186b16c5fe0b67f08d33f06c3bbc84
Vincent Heuken <me@vincentheuken.com>
2015-06-02 04:55:13 -0700

    removed unnecessary functions from youtubeplaylist_scraper

--- lib/video_info/providers/youtubeplaylist_scraper.rb
+++ lib/video_info/providers/youtubeplaylist_scraper.rb
@@ -5,26 +5,6 @@ require 'open_uri_redirections'
 class VideoInfo
   module Providers
     module YoutubePlaylistScraper
-      def description
-        meta_nodes = data.css('meta')
-
-        description_node = meta_nodes.detect do |m|
-          m.attr('name').value == 'description'
-        end
-
-        description_node.attr('content').value
-      end
-
-      def title
-        meta_nodes = data.css('meta')
-
-        title_node = meta_nodes.detect do |m|
-          m.attr('name').value == 'title'
-        end
-
-        title_node.attr('content').value
-      end
-
       def videos
         raise(NotImplementedError, 'To access videos, you must provide an API key to VideoInfo.provider_api_keys')
       end
@@ -39,20 +19,6 @@ class VideoInfo
 
       def thumbnail_large
         thumbnail_medium.sub('mqdefault.jpg', 'hqdefault.jpg')
-      end
-
-      private
-
-      def available?
-        !data.css('div#page').attr('class')[0].value.include?('oops-content')
-      end
-
-      def _set_data_from_api(api_url = _api_url)
-        Oga.parse_html(open(api_url, :allow_redirections => :safe))
-      end
-
-      def _api_url
-        @url
       end
     end
   end
