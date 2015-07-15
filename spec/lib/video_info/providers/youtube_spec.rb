0c1283603595718b88e90b50029cffc4b3465ca3
Rob Guilfoyle <robg2310@gmail.com>
2015-07-14 23:37:47 -0400

    adds the YouTube channels API to fetch providers

--- spec/lib/video_info/providers/youtube_spec.rb
+++ spec/lib/video_info/providers/youtube_spec.rb
@@ -117,6 +117,8 @@ describe VideoInfo::Providers::Youtube do
         it { is_expected.to be == 0 }
       end
 
+
+
     end
 
     context "with 'video no longer available due to a copyright claim' video", :vcr do
@@ -211,6 +213,17 @@ describe VideoInfo::Providers::Youtube do
       subject { super().view_count }
       it { is_expected.to be > 4000 }
     end
+
+    describe '#author' do
+      subject { super().author }
+      it { is_expected.to eql 'Cherry Bloom' }
+    end
+
+    describe '#author_thumbnail' do
+      subject { super().author_thumbnail }
+      it { is_expected.to eql 'https://yt3.ggpht.com/-7rhnfdQaI3k/AAAAAAAAAAI/AAAAAAAAAAA/eMJZ5HBukCQ/s88-c-k-no/photo.jpg' }
+    end
+
   end
 
   context "with video oQ49W_xKzKA", :vcr do
