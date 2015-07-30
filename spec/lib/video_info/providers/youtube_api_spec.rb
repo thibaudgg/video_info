7a91114d29902cd23b420fa22261c3e4af7335d4
Vincent Heuken <me@vincentheuken.com>
2015-07-30 08:28:20 -0700

    fixed spec

--- spec/lib/video_info/providers/youtube_api_spec.rb
+++ spec/lib/video_info/providers/youtube_api_spec.rb
@@ -123,7 +123,7 @@ describe VideoInfo::Providers::Youtube do
 
       describe '#available?' do
         subject { super().available? }
-        it { is_expected.to be_falsey }
+        it { is_expected.to be_true }
       end
     end
   end
