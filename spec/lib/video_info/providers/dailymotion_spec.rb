45981e5ae1410b924b2dce94563d7f2f3294e058
Vincent Heuken <me@vincentheuken.com>
2016-03-11 13:33:51 -0800

    cleaned up dailymotion style errors

--- spec/lib/video_info/providers/dailymotion_spec.rb
+++ spec/lib/video_info/providers/dailymotion_spec.rb
@@ -2,28 +2,27 @@
 require 'spec_helper'
 
 describe VideoInfo::Providers::Dailymotion do
-
-  describe ".usable?" do
+  describe '.usable?' do
     subject { VideoInfo::Providers::Dailymotion.usable?(url) }
 
-    context "with dailymotion url" do
+    context 'with dailymotion url' do
       let(:url) { 'http://www.dailymotion.com/video/x7lni3' }
       it { is_expected.to be_truthy }
     end
 
-    context "with dai.ly url" do
+    context 'with dai.ly url' do
       let(:url) { 'http://dai.ly/video/x7lni3' }
       it { is_expected.to be_truthy }
     end
 
-    context "with other url" do
+    context 'with other url' do
       let(:url) { 'http://www.youtube.com/x7lni3' }
       it { is_expected.to be_falsey }
     end
   end
 
-  describe "#available?" do
-    context "with valid video", :vcr do
+  describe '#available?' do
+    context 'with valid video', :vcr do
       subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
 
       describe '#available?' do
@@ -32,7 +31,7 @@ describe VideoInfo::Providers::Dailymotion do
       end
     end
 
-    context "with 'This video does not exist or has been deleted.", :vcr do
+    context 'with invalid video url', :vcr do
       subject { VideoInfo.new('http://www.dailymotion.com/video/inValIdiD') }
 
       describe '#available?' do
@@ -42,7 +41,7 @@ describe VideoInfo::Providers::Dailymotion do
     end
   end
 
-  context "with video x7lni3", :vcr do
+  context 'with video x7lni3', :vcr do
     subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
 
     describe '#provider' do
@@ -67,7 +66,9 @@ describe VideoInfo::Providers::Dailymotion do
 
     describe '#embed_code' do
       subject { super().embed_code }
-      it { is_expected.to eq '<iframe src="//www.dailymotion.com/embed/video/x7lni3?autoplay=0" frameborder="0"></iframe>' }
+      embed_code = '<iframe src="//www.dailymotion.com/embed/video/x7lni3?' \
+                   'autoplay=0" frameborder="0"></iframe>'
+      it { is_expected.to eq embed_code }
     end
 
     describe '#title' do
@@ -77,17 +78,23 @@ describe VideoInfo::Providers::Dailymotion do
 
     describe '#description' do
       subject { super().description }
-      it { is_expected.to eq 'Super Rémi Kart est un jeu vidéo de course développé et édité par N\'Importe Quoi TV. Sa principale originalité vient de sa réalité...<br />L\'aventure continue sur www.nimportequi.com' }
+      description_text = 'Super Rémi Kart est un jeu vidéo de course ' \
+                         'développé et édité par N\'Importe Quoi TV. ' \
+                         'Sa principale originalité vient de sa réalité' \
+                         '...<br />L\'aventure continue sur www.nimportequi.com'
+      it { is_expected.to eq description_text }
     end
 
     describe '#keywords' do
       subject { super().keywords }
-      it { is_expected.to eq ["rémi","remi","gaillard","imposteur","imposture","mario","bros","kart","nintendo","jeux"] }
+      keywords_list = %w(rémi remi gaillard imposteur imposture
+                         mario bros kart nintendo jeux)
+      it { is_expected.to eq keywords_list }
     end
 
     describe '#duration' do
       subject { super().duration }
-      it { is_expected.to eq 136}
+      it { is_expected.to eq 136 }
     end
 
     describe '#width' do
@@ -102,7 +109,9 @@ describe VideoInfo::Providers::Dailymotion do
 
     describe '#date' do
       subject { super().date }
-      it { is_expected.to eq Time.parse('2008-12-03 16:29:31 UTC', Time.now.utc) }
+      it 'should show correct date' do
+        is_expected.to eq Time.parse('2008-12-03 16:29:31 UTC', Time.now.utc)
+      end
     end
 
     describe '#thumbnail_small' do
@@ -122,25 +131,35 @@ describe VideoInfo::Providers::Dailymotion do
 
     describe '#view_count' do
       subject { super().view_count }
-      it { is_expected.to be > 10000000 }
+      it { is_expected.to be > 10_000_000 }
     end
   end
 
-  context "with video x7lni3 and url_attributes", :vcr do
+  context 'with video x7lni3 and url_attributes', :vcr do
     subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
 
-    it { expect(subject.embed_code(url_attributes: { autoplay: 1 })).to match(/autoplay=1/) }
+    it do
+      expected = expect(subject.embed_code(url_attributes: { autoplay: 1 }))
+      expected.to match(/autoplay=1/)
+    end
   end
 
-  context "with video x7lni3 and iframe_attributes", :vcr do
+  context 'with video x7lni3 and iframe_attributes', :vcr do
     subject { VideoInfo.new('http://www.dailymotion.com/video/x7lni3') }
 
-    it { expect(subject.embed_code(iframe_attributes: { width: 800, height: 600 })).to match(/width="800"/) }
-    it { expect(subject.embed_code(iframe_attributes: { width: 800, height: 600 })).to match(/height="600"/) }
+    it 'should properly apply dimensions attributes' do
+      dimensions = { width: 800, height: 600 }
+      expected = expect(subject.embed_code(iframe_attributes: dimensions))
+      expected.to match(/width="800"/)
+      expected.to match(/height="600"/)
+    end
   end
 
-  context "with video x7lni3 in text", :vcr do
-    subject { VideoInfo.new('<a href="http://www.dailymotion.com/video/x7lni3">http://www.dailymotion.com/video/x7lni3</a>') }
+  context 'with video x7lni3 in text', :vcr do
+    url_in_text = '<a href="http://www.dailymotion.com/video/x7lni3">' \
+                  'http://www.dailymotion.com/video/x7lni3</a>'
+
+    subject { VideoInfo.new(url_in_text) }
 
     describe '#provider' do
       subject { super().provider }
@@ -152,5 +171,4 @@ describe VideoInfo::Providers::Dailymotion do
       it { is_expected.to eq 'x7lni3' }
     end
   end
-
 end
