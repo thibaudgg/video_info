981ecf1f1df325761683207bc207eb75f637c3ac
Joao C Vieira <joaocv3@gmail.com>
2020-09-06 06:26:48 -0300

    Fix youtube scraper description spec expected value

--- spec/lib/video_info/providers/youtube_scraper_spec.rb
+++ spec/lib/video_info/providers/youtube_scraper_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe VideoInfo::Providers::Youtube do
-  context 'with video WVsj2pS-zFY' do
+  context 'with a video without description', :vcr do
     subject { VideoInfo.new('http://www.youtube.com/watch?v=WVsj2pS-zFY') }
 
     describe '#description' do
       subject { super().description }
-      it { is_expected.to eq nil }
+      it { is_expected.to eq " " }
     end
   end
 end
